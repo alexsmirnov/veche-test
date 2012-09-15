4d5a324c34893c0cdfc72ef53b63e158d5ba3145
Alex Smirnov <alsmirnnov@gmail.com>
2012-09-15 00:45:07 -0700

    move callback verification to the core project

--- src/main/java/com/netoprise/veche/test/LoginBean.java
+++ src/main/java/com/netoprise/veche/test/LoginBean.java
@@ -1,75 +1,74 @@
 package com.netoprise.veche.test;
 
+import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Model;
+import javax.enterprise.inject.Produces;
 import javax.faces.context.FacesContext;
 import javax.faces.event.PhaseEvent;
 import javax.faces.event.PhaseId;
 import javax.faces.event.PhaseListener;
 import javax.inject.Inject;
+import javax.inject.Named;
 
 import com.google.common.base.Optional;
 import com.netoprise.veche.CallbackVerifier;
 import com.netoprise.veche.OAuthSession;
+import com.netoprise.veche.ProviderId;
 import com.netoprise.veche.SessionOrDenied;
 import com.netoprise.veche.cdi.VerifierCache;
+import com.netoprise.veche.faces.CallbackAction;
+import com.netoprise.veche.faces.CallbackMessages;
 import com.netoprise.veche.faces.PageActionListener;
+import com.netoprise.veche.faces.SessionAction;
 
 @Model
-public class LoginBean extends PageActionListener {
+public class LoginBean implements SessionAction<OAuthSession>, CallbackMessages {
 
-    private int scope = Integer.MIN_VALUE;
 
     private String message = "INVOKE_APPLICATION phase missed";
 
+
+    private CallbackAction<OAuthSession> callbackAction;
+    
+    public LoginBean() {
+	// default constructor required by bean contract
+    }
+
     @Inject
-    VerifierCache cache;
-
-    /**
-     * @return the scope
-     */
-    public int getScope() {
-	return this.scope;
+    public LoginBean(VerifierCache cache) {
+	callbackAction = new CallbackAction<OAuthSession>(cache, this, this);
     }
-
-    /**
-     * @param scope
-     *            the scope to set
-     */
-    public void setScope(int scope) {
-	this.scope = scope;
-    }
-
+    
     public String getMessage() {
 	return this.message;
     }
 
+
+    @Produces
+    @Named("callbackAction")
+    public CallbackAction<OAuthSession> getCallbackAction(){
+	return callbackAction;
+    }
+    
     @Override
-    public String performAction() {
-	String result;
-	if (this.scope > Integer.MIN_VALUE) {
-	    FacesContext facesContext = FacesContext.getCurrentInstance();
-	    Optional<CallbackVerifier<OAuthSession>> callbackVerifierOption = cache.getVerifier(scope);
-	    if (callbackVerifierOption.isPresent()) {
-		CallbackVerifier<OAuthSession> callbackVerifier = callbackVerifierOption.get();
-		SessionOrDenied<OAuthSession> sessionOrDenied = callbackVerifier.verify(facesContext.getExternalContext()
-		        .getRequestParameterMap());
-		if (sessionOrDenied.isAuthtorized()) {
-		    OAuthSession oAuthSession = sessionOrDenied.getSession();
-		    this.message = "Verified " + callbackVerifier.getProviderId() + "user id: " + oAuthSession.getClientId();
-		    result = "success";
-		} else {
-		    this.message = "oAuth error: " + sessionOrDenied.getErrorDescription();
-		    result = "denied";
-		}
-	    } else {
-		this.message = "Missed verifier in cache";
-		result = "verifierError";
-	    }
-	} else {
+    public void denied(FacesContext facesContext, ProviderId providerId, String errorDescription) {
+	    this.message = "oAuth error: " + errorDescription;
+    }
+
+    @Override
+    public void missedVerifier(FacesContext facesContext) {
+	this.message = "Missed verifier in cache";
+    }
+
+    @Override
+    public void missedScope(FacesContext facesContext) {
 	    this.message = "Missed scope param";
-	    result = "missedScope";
-	}
-        return result;
+    }
+
+    @Override
+    public String action(OAuthSession session) {
+	    this.message = "Verified " + session.getProviderId() + "user id: " + session.getClientId();
+	    return "success";
     }
 
 }
